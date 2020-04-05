8dacd0ca0db9e8bb7694b0fc49ae13580aa10762
SpiralP <spiral.palmer@gmail.com>
2020-04-04 17:01:06 -0700

    init

---
+++ src/lib.rs
@@ -0,0 +1,29 @@
+use classicube_sys::*;
+use std::{os::raw::c_int, ptr};
+
+extern "C" fn init() {
+    let owned_string = OwnedString::new("hello from rust!");
+
+    unsafe {
+        Chat_Add(owned_string.as_cc_string());
+    }
+}
+
+#[no_mangle]
+pub static Plugin_ApiVersion: c_int = 1;
+
+#[no_mangle]
+pub static mut Plugin_Component: IGameComponent = IGameComponent {
+    /* Called when the game is being loaded. */
+    Init: Some(init),
+    /* Called when the component is being freed. (e.g. due to game being closed) */
+    Free: None,
+    /* Called to reset the component's state. (e.g. reconnecting to server) */
+    Reset: None,
+    /* Called to update the component's state when the user begins loading a new map. */
+    OnNewMap: None,
+    /* Called to update the component's state when the user has finished loading a new map. */
+    OnNewMapLoaded: None,
+    /* Next component in linked list of components. */
+    next: ptr::null_mut(),
+};

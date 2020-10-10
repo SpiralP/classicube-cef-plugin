cf164000a6d0df4a4084d3f8a2709139ebce9010
SpiralP <spiral.palmer@gmail.com>
2020-10-09 18:41:54 -0700

    actions: fix release body

--- .github/format-release.js
+++ .github/format-release.js
@@ -1,19 +1,11 @@
 const analysis = process.argv[2];
-const version = process.env.GITHUB_REF.replace(/^refs\/tags\//, "").replace(
-  /^v/,
-  ""
+
+console.log(
+  analysis
+    .split(",")
+    .map((line) => {
+      const [file, url] = line.split("=");
+      return `\`${file}\`: [VirusTotal analysis](${url})`;
+    })
+    .join("\n")
 );
-
-const lines = [`Release version ${version}`, ""];
-
-analysis
-  .split(",")
-  .map((line) => {
-    const [file, url] = line.split("=");
-    return `\`${file}\`: [VirusTotal analysis](${url})`;
-  })
-  .forEach((line) => {
-    lines.push(line);
-  });
-
-console.log(lines.join("\n"));

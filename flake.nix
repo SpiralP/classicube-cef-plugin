865f247f69fa2585cd42986e389c05bd23aff156
SpiralP <spiral.palmer@gmail.com>
2024-10-01 13:07:25 -0700

    Update lock files

--- flake.nix
+++ flake.nix
@@ -82,7 +82,7 @@
               outputHashes = {
                 "async-dispatcher-0.1.0" = "sha256-rqpQ176/PnI9vvPrwQvK3GJbryjb3hHkb+o1RyCZ3Vg=";
                 "clap-4.2.7" = "sha256-PccqMT2KltTC2gVL9/xfCNFOAu3+6ash9HqM/TkpgmU=";
-                "classicube-helpers-2.0.0+classicube.1.3.6" = "sha256-mGRzuvxKXKBxKgZuLkc7qzmABIj9uADuxAkahR8e840=";
+                "classicube-helpers-2.0.0+classicube.1.3.6" = "sha256-NgpBwlYCJyBV+oKyVyU5ueI9AubEZ0tsrsH+eQFF3Xk=";
               };
             };
 

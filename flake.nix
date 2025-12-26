3c4a617f82c6610077c4a9a79c38b44a6be468c9
SpiralP <2898730+SpiralP@users.noreply.github.com>
2025-12-26 19:14:19 +0000

    fix(cef_binary): update cef_binary to `143.0.13+g30cb3bd+chromium-143.0.7499.170`

--- flake.nix
+++ flake.nix
@@ -30,7 +30,7 @@
                 # "aarch64-darwin" = { platformUrl = "macosarm64"; projectArchCmake = "arm64"; };
               };
 
-              platforms."x86_64-linux".hash = "sha256-fhg6QJZ4GlmQB4PkBILAbl5f3acspAV/J/WtpYdVCcs=";
+              platforms."x86_64-linux".hash = "sha256-qoyvCmbx5fkcXg/gkdufLszL2taC1cgO6Wna0ksk8ZA=";
               # platforms."aarch64-linux".hash = "";
               # platforms."armv7l-linux".hash = "";
               # platforms."x86_64-darwin".hash = "";

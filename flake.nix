5ffa200c0f8d3adb162c5473c29f98615c46ac32
SpiralP <spiral.palmer@gmail.com>
2026-05-18 16:37:15 -0700

    ci: migrate to build-classicube-plugin composite action
    Mirror classicube-cef-loader-plugin's c64f0fc2 refactor.
    
    - collapse per-OS build jobs into matrix jobs that call the shared
      build-classicube-plugin composite action; CEF-specific steps
      (cef_binary fetch, MSBuild setup, chromium runtime libs) stay as
      pre-steps before the composite call
    - release.yml now runs a pre-publish smoke-test build, then lets
      semantic-release dispatch build.yml on the release tag and attach
      the resulting artifacts via @semantic-release/github
    - migrate flake.nix to flake-utils.lib.makeOutputs + makeRustPackage;
      keep the custom cef_binary derivation; bump nixpkgs to nixos-25.11;
      drop the enableDebugging stdenv wrapper on the debug package (the
      helper does not expose stdenv override)
    - migrate Cross.toml's docker image map to package.metadata.cross
      in Cargo.toml; drop the unused x86_64 entry
    - drop the VirusTotal scan and the .pdb upload (PDBs aren't a
      cargo metadata target; can be revisited via an upstream change)
    - drop [package.metadata.release]; semantic-release owns version
      bumps and commit messages now
    
    BREAKING CHANGE: the `cef` binary release asset is renamed from
    `cef-<os>-<arch>` to `cef_<os>_<arch>` to match the cdylib naming
    convention now produced by the shared composite action. Downstream
    consumers (classicube-cef-loader-plugin) must update their download
    URLs in lockstep.

--- flake.nix
+++ flake.nix
@@ -1,50 +1,40 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    flake-utils.url = "github:SpiralP/nix-flake-utils";
   };
 
-  outputs = { self, nixpkgs }:
-    let
-      inherit (nixpkgs) lib;
-
-      rustManifest = lib.importTOML ./Cargo.toml;
-
-      revSuffix = lib.optionalString (self ? dirtyShortRev)
-        "-${self.dirtyShortRev}";
-
-      makePackages = (system: dev:
+  outputs = inputs@{ flake-utils, ... }:
+    flake-utils.lib.makeOutputs inputs
+      ({ lib, pkgs, system, dev, makeRustPackage, ... }:
         let
-          pkgs = import nixpkgs {
-            inherit system;
+          platforms = {
+            "x86_64-linux" = {
+              platformUrl = "linux64";
+              projectArchCmake = "x86_64";
+              hash = "sha256-kBfUz9jrBxLbELvgrU2gbgEDC1z0y2jkhhdt7QJY4ZE=";
+            };
+            # TODO test if arm builds/works before adding these
+            # "aarch64-linux" = { platformUrl = "linuxarm64"; projectArchCmake = "arm64"; hash = ""; };
+            # "armv7l-linux"  = { platformUrl = "linuxarm";   projectArchCmake = "arm";   hash = ""; };
+            # TODO need to remove unsupported rpath's on libcef
+            # "x86_64-darwin"  = { platformUrl = "macosx64";   projectArchCmake = "x86_64"; hash = ""; };
+            # "aarch64-darwin" = { platformUrl = "macosarm64"; projectArchCmake = "arm64";  hash = ""; };
           };
 
-          makeCefBinaryAttrs =
+          supported = builtins.hasAttr system platforms;
+
+          makeCefBinaryAttrs = debug: prev:
             let
-              platforms = {
-                "x86_64-linux" = { platformUrl = "linux64"; projectArchCmake = "x86_64"; };
-                # TODO test if arm builds/works before adding these
-                # "aarch64-linux" = { platformUrl = "linuxarm64"; projectArchCmake = "arm64"; };
-                # "armv7l-linux" = { platformUrl = "linuxarm"; projectArchCmake = "arm"; };
-                # TODO need to remove unsupported rpath's on libcef
-                # "x86_64-darwin" = { platformUrl = "macosx64"; projectArchCmake = "x86_64"; };
-                # "aarch64-darwin" = { platformUrl = "macosarm64"; projectArchCmake = "arm64"; };
-              };
-
-              platforms."x86_64-linux".hash = "sha256-kBfUz9jrBxLbELvgrU2gbgEDC1z0y2jkhhdt7QJY4ZE=";
-              # platforms."aarch64-linux".hash = "";
-              # platforms."armv7l-linux".hash = "";
-              # platforms."x86_64-darwin".hash = "";
-              # platforms."aarch64-darwin".hash = "";
-
-              inherit (platforms.${pkgs.stdenv.hostPlatform.system}) platformUrl projectArchCmake hash;
+              p = platforms.${system};
             in
-            (debug: prev: (rec {
+            (rec {
               version = lib.strings.trim (builtins.readFile ./cef_binary_version);
 
               src = pkgs.fetchzip {
-                inherit hash;
+                inherit (p) hash;
                 name = "cef_binary-${version}";
-                url = "https://cef-builds.spotifycdn.com/cef_binary_${version}_${platformUrl}.tar.bz2";
+                url = "https://cef-builds.spotifycdn.com/cef_binary_${version}_${p.platformUrl}.tar.bz2";
               };
 
               installPhase = ''
@@ -72,125 +62,72 @@
 
               cmakeFlags =
                 if builtins.length (prev.cmakeFlags or [ ]) == 0
-                then [ "-DPROJECT_ARCH=${projectArchCmake}" ]
+                then [ "-DPROJECT_ARCH=${p.projectArchCmake}" ]
                 else throw "cmakeFlags changed?";
 
               meta = prev.meta // {
                 platforms = builtins.attrNames platforms;
               };
-            }) // (
-              lib.attrsets.optionalAttrs debug {
-                pname = "${prev.pname}-debug";
-
-                cmakeBuildType = "Debug";
-
-                hardeningDisable = [ "all" ];
-              }
-            ));
-
-          makeDefaultAttrs = (cef_binary: rec {
-            pname = rustManifest.package.name;
-            version = rustManifest.package.version + revSuffix;
-
-            src = lib.sourceByRegex ./. [
-              "^\.cargo(/.*)?$"
-              "^build\.rs$"
-              "^Cargo\.(lock|toml)$"
-              "^cef_interface(/.*)?$"
-              "^src(/.*)?$"
-            ];
-
-            cargoLock = {
-              lockFile = ./Cargo.lock;
-              allowBuiltinFetchGit = true;
+            }) // lib.attrsets.optionalAttrs debug {
+              pname = "${prev.pname}-debug";
+              cmakeBuildType = "Debug";
+              hardeningDisable = [ "all" ];
             };
+
+          cef_binary = pkgs.cef-binary.overrideAttrs (makeCefBinaryAttrs false);
+          cef_binary_debug = pkgs.cef-binary.overrideAttrs (makeCefBinaryAttrs true);
+
+          src = lib.sourceByRegex ./. [
+            "^\.cargo(/.*)?$"
+            "^build\.rs$"
+            "^Cargo\.(lock|toml)$"
+            "^cef_interface(/.*)?$"
+            "^src(/.*)?$"
+          ];
+
+          makeArgs = cb: {
+            inherit src;
 
             nativeBuildInputs = with pkgs; [
               cmake
               pkg-config
               rustPlatform.bindgenHook
-            ] ++ (if dev then
-              with pkgs; ([
-                cargo-release
-                clippy
-                (rustfmt.override { asNightly = true; })
-                rust-analyzer
-              ]) else [ ]);
+            ];
 
-            LIBCEF_LIB_DIR = "${cef_binary}/lib";
-            LIBCEF_INCLUDE_DIR = "${cef_binary}/include";
-            LIBCEF_DLL_WRAPPER_LIB_DIR = LIBCEF_LIB_DIR;
+            buildInputs = with pkgs; [ cb openssl zstd ];
+
+            LIBCEF_LIB_DIR = "${cb}/lib";
+            LIBCEF_INCLUDE_DIR = "${cb}/include";
+            LIBCEF_DLL_WRAPPER_LIB_DIR = "${cb}/lib";
 
             ZSTD_SYS_USE_PKG_CONFIG = "1";
             OPENSSL_LIB_DIR = "${lib.getLib pkgs.openssl}/lib";
             OPENSSL_INCLUDE_DIR = "${lib.getDev pkgs.openssl}/include";
-            buildInputs = with pkgs; [
-              cef_binary
-              openssl
-              zstd
-            ];
 
             dontUseCargoParallelTests = true;
+            useNextest = true;
 
             postFixup = ''
               mv -v $out/lib $out/plugins
               mv -v $out/bin $out/cef
 
               mkdir -vp $out/cef/cef_binary
-              ln -vs ${cef_binary}/lib/* $out/cef/cef_binary/
+              ln -vs ${cb}/lib/* $out/cef/cef_binary/
             '';
 
             # so that when developing we don't get spammed with
             # "warning _FORTIFY_SOURCE requires compiling with optimization (-O)"
-            hardeningDisable =
-              if dev
-              then [ "all" ]
-              else [ ];
-          });
+            hardeningDisable = lib.optionals dev [ "all" ];
+          };
         in
-        rec {
-          default = pkgs.rustPlatform.buildRustPackage (makeDefaultAttrs cef_binary);
+        lib.optionalAttrs supported {
+          default = makeRustPackage pkgs (makeArgs cef_binary);
 
-          debug = (pkgs.enableDebugging {
-            inherit (pkgs) stdenv;
-            override = (attrs: pkgs.makeRustPlatform ({
-              inherit (pkgs) rustc cargo;
-            } // attrs));
-          }).buildRustPackage (
-            let
-              attrs = makeDefaultAttrs cef_binary_debug;
-            in
-            (attrs // {
-              pname = "${attrs.pname}-debug";
+          debug = makeRustPackage pkgs (makeArgs cef_binary_debug // {
+            buildType = "debug";
+            hardeningDisable = [ "all" ];
+          });
 
-              buildType = "debug";
-
-              hardeningDisable = [ "all" ];
-            })
-          );
-
-          cef_binary = pkgs.cef-binary.overrideAttrs (makeCefBinaryAttrs false);
-
-          cef_binary_debug = (pkgs.enableDebugging pkgs.cef-binary).overrideAttrs (makeCefBinaryAttrs true);
-        }
-      );
-    in
-    builtins.foldl' lib.recursiveUpdate { } (builtins.map
-      (system: {
-        devShells.${system} = makePackages system true;
-        packages.${system} = makePackages system false;
-      })
-      [
-        "x86_64-linux"
-        # TODO
-        "aarch64-linux"
-        # TODO
-        # cef "Linux ARM" is armv7
-        "armv7l-linux"
-
-        # TODO
-        "x86_64-darwin"
-        # TODO
-        "aarch64-darwin"
-      ]);
+          inherit cef_binary cef_binary_debug;
+        });
 }

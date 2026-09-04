a1e9d00ab20206ae14eb33d9744f547344ce75b7
SpiralP <2898730+SpiralP@users.noreply.github.com>
2026-09-04 19:22:11 +0000

    fix(cef_binary): update cef_binary to `151.3.24+g2384915+chromium-151.0.7922.174`

--- flake.nix
+++ flake.nix
@@ -12,7 +12,7 @@
             "x86_64-linux" = {
               platformUrl = "linux64";
               projectArchCmake = "x86_64";
-              hash = "sha256-ExokI2UuUBWC9M91HMfFEZXeSI05Dx4hX33vPkWwMjU=";
+              hash = "sha256-sGZ5HdmmnClZoageOSVrVHVtSNE1l1lNa26Xs//qFV0=";
             };
             # TODO test if arm builds/works before adding these
             # "aarch64-linux" = { platformUrl = "linuxarm64"; projectArchCmake = "arm64"; hash = ""; };
